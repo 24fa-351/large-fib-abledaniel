64cb8c09e6811c42ba3a3be1cd574ca6be0e243b
Abel Daniel <123517214+abledaniel@users.noreply.github.com>
2024-09-22 15:59:12 -0700

    Finished

--- fib.c
+++ fib.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
-   return 0;
+//write in C fibonnaci recursively and write it iteratively 
+//then accept the command line argument read the file then calculate the value to the Nth fibbnoaci
+
+uint64_t fibo_iterative(uint64_t f) {
+   if (f <= 1){
+    return f;
+   } 
+   uint64_t a = 0;
+   uint64_t b = 1;
+   for (int i = 2; i <= f; i++){
+      uint64_t origin = a;
+      a = b;
+      b = origin + b;
+   }
+    return b;
+}
+
+uint64_t fibo_recursive(uint64_t f){
+    if (f <= 1){
+        return f;
+    }
+    return fibo_recursive(f-1) + fibo_recursive(f-2);
+}
+
+
+int main(int argc, char *cmd[]) {
+    if (argc != 4) {
+        return 1;
+    }
+
+    uint64_t input_number = atoi(cmd[1]), file_number;
+    FILE *file = fopen(cmd[3], "r");
+    fscanf(file, "%llu", &file_number);
+
+    uint64_t N = input_number + file_number;
+    uint64_t result = 0;
+    char fib = cmd[2][0];
+    if (fib == 'r'){
+        result = fibo_recursive(N);
+    }
+    if (fib == 'i'){
+        result = fibo_iterative(N);
+    }
+    fscanf("%llu\n", result);
+
+    return 0;
 }
